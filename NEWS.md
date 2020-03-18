9bd9f60b73a1147713b4ab27665a8665238c59db
Scott Chamberlain <myrmecocystus@gmail.com>
2020-03-17 18:26:37 -0700

    update news, cran comments, codemeta.json

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,12 @@
+rbraries 0.2.0
+==============
+
+### NEW FEATURES
+
+* tell the user the exact name of the environment variable used in the package (#1) thanks @jonocarroll
+* use vcr for tests (#4)
+
+
 rbraries 0.1.0
 ==============
 
--- R/rbraries-package.R
+++ R/rbraries-package.R
@@ -1,20 +1,20 @@
 #' Interface to the libraries.io API
-#' 
-#' Client for the libraries.io API, a platform for "helping 
-#' you find new open source libraries, modules and frameworks 
+#'
+#' Client for the libraries.io API, a platform for "helping
+#' you find new open source libraries, modules and frameworks
 #' and keep track of ones you depend upon."
-#' 
+#'
 #' @section Auth:
-#' You need an API key to use this package. Get one by logging in to 
-#' Libraries.io (they have GitHub login and others), then go to your 
+#' You need an API key to use this package. Get one by logging in to
+#' Libraries.io (they have GitHub login and others), then go to your
 #' Settings page, then scroll down to API key section and grab your
-#' key. 
-#' 
+#' key.
+#'
 #' You can pass the key into function calls, but that's not recommended.
-#' Store your key by putting a `LIBRARIES_IO_KEY` entry in your .Renviron file or similar file like
-#' e.g. .zshrc or .bash_profile, etc. We'll grab that key so that you 
-#' don't have to pass it into each function call.
-#' 
+#' Store your key by putting a `LIBRARIES_IO_KEY` entry in your .Renviron
+#' file or similar file like e.g. .zshrc or .bash_profile, etc. We'll grab
+#' that key so that you don't have to pass it into each function call.
+#'
 #' @importFrom jsonlite fromJSON
 #' @importFrom crul HttpClient
 #' @name rbraries-package
